f8d7572987a4775013a59d03bfe965910aba4e9a
IvanTsiak <ivanzjak2007@gmail.com>
2026-05-27 23:56:18 +0300

    initial commit

---
+++ DbHelper.cs
@@ -0,0 +1,36 @@
+﻿using System.Data;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace FestivalsBD;
+
+public class DbHelper
+{
+    private static readonly string ConnectionString;
+
+    static DbHelper()
+    {
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+        IConfiguration config = builder.Build();
+        ConnectionString = config.GetConnectionString("DefaultConnection");
+    }
+
+    public static DataTable ExecuteQuery(string query)
+    {
+        using (SqlConnection connection = new SqlConnection(ConnectionString))
+        {
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+                {
+                    DataTable dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+                    return dataTable;
+                }
+            }
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,9 @@
+namespace FestivalsBD;
+
+public partial class Form1 : Form
+{
+    public Form1()
+    {
+        InitializeComponent();
+    }
+}
